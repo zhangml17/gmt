7faf92f4e6b884d16c1dd19db37b48ccece44a47
zhangml17 <meilongyx@163.com>
2018-10-15 08:37:58 +0200

    k8s install

---
+++ k8s-install-zml/install.sh
@@ -0,0 +1,69 @@
+#! /bin/bash
+
+#设置默认参数
+DEFAULT_CNI=flannel
+DEFAULT_HA=nginx
+DEFAULT_PROXY=iptables
+DEFAULT_VERSION=v1.11.1
+DEFAULT_REUSE=true
+#$0:获取命令行的第一个参数，即要执行的文本名
+usage(){
+cat << EOF
+usage:$0 [ -m MASTER(S) ] [ -n NODE(S) ] [ -i VIRTUAL-IP ] [ -p PASSORD ]
+       [ -c CNI ] [ -a HA-STRATEGY ] [ -x PROXY-STATEGY ] [ -v KUBE-VERSION ]
+       [ -r REUSE-MASTER-AS-NODE ] [ -k SCKEY ]
+       [ -b BRANCH ] [ -s SCRIPT-BRANCH ]
+use to deploy Kubernetes.
+    -m : Specify the IP address(es) of Master node(s). If multiple, set the masters in term of csv, 
+         as 'master-ip-1,master-ip-2,master-ip-3'.
+    -p : Specify the uniform password of hosts. 
+    advanced setting:
+    -n : Specify the IP address(es) of Node node(s). If multiple, set the nodes in term of csv, 
+         as 'node-ip-1,node-ip-2,node-ip-3'.
+         If not specified, no nodes would be installed.
+    -a : Specify the HA strategy, for instance: "nginx" or "vip".  
+         If not specified, use "$DEFAULT_HA" by default.
+    -i : Specify the virtual IP address. 
+    -c : Specify the CNI strategy, for instance: "flannel" or "calico".  
+         If not specified, use "$DEFAULT_CNI" by default.
+    -x : Specify the proxy strategy, for instance: "iptables" or "ipvs".  
+         If not specified, use "$DEFAULT_PROXY" by default.
+    -v : Specify the version of Kubernetes to install.  
+         If not specified, install "$DEFAULT_VERSION" by default.
+    -r : Define if reusing master as node, which means installing node components on master or not.
+         If not specified, reuse master as node by default. 
+    -k : Specify the SCKEY of the user to send note to. If multiple, set the keys in term of csv, 
+         as 'key-1,key-2,key-3'. 
+    debug setting:
+    -b : Specify the branch of code. 
+         If not specified, use "master" by default.
+    -s : Specify the branch of stage scripts. 
+         If not specified, set the value of -b by default.
+This script should run on a Master (to be) node.
+EOF
+exit -1
+}
+
+while getopts "um:p:n:a:i:c:x:v:rk:b:s:" opt;do 
+  case "$opt" in
+    u) usage
+    ;;
+    m) MASTER=$OPTARG;;
+    p) PASSWD=$OPTARG;;
+    n) ONLY_NODE=$OPTARG;;
+    a) HA=$OPTARG;;
+    i) VIP=$OPTARG;;
+    c) CNI=$OPTARG;;
+    x) PROXY=$OPTARG;;
+    v) VERSION=$OPTARG;;
+    r) REUSE=FALSE;;
+    k) SCKEY=$OPTARG;;
+    b) BRANCH=$OPTARG;;
+    s) STAGES_BRANCH=$OPTARG;;
+    ?) 
+       echo "unkonw argument"
+       exit -1
+     ;;
+  esac
+done
+[ -z "$*"] && usage
---
+++ k8s-install-zml/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+usage() {
+    echo "Usage:"
+    echo "  test.sh [-j JAVA_DIR] [-m MAVEN_DIR]"
+    echo "Description:"
+    echo "    JAVA_DIR, the path of java."
+    echo "    MAVEN_DIR, the path of maven."
+    exit -1
+}
+
+upload="false"
+
+while getopts 'h:j:m:u' OPT; do
+    case $OPT in
+        j) JAVA_DIR="$OPTARG";;
+        m) MAVEN_DIR="$OPTARG";;
+        u) upload="true";;
+        h) usage;;
+        ?) usage;;
+    esac
+done
+
+echo $JAVA_DIR
+echo $MAVEN_DIR
+echo $upload
